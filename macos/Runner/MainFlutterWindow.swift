ee7275d257c3cc2828fed769c34e75ac57add5df
Saurabh <111645017+saurabhsargar@users.noreply.github.com>
2025-07-08 18:12:43 +0530

    Improve macos/Runner/MainFlutterWindow.swift - automated code enhancements

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -1,9 +1,13 @@
 import Cocoa
 import FlutterMacOS
+import Foundation
 
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController()
+    let windowFrame = self.frame
+    self.contentViewController = flutterViewController
+    self.setFrame(windowFrame, display: true)
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
