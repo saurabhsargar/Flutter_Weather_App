6d39a5dbd7838a5dd2505820eb5c06ececc61d83
Saurabh <111645017+saurabhsargar@users.noreply.github.com>
2025-07-08 18:11:22 +0530

    Improve lib/bloc/weather_bloc.dart - automated code enhancements

--- lib/bloc/weather_bloc.dart
+++ lib/bloc/weather_bloc.dart
@@ -1,8 +1,11 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:equatable/equatable.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:weather/weather.dart';
+import 'package:weather/weather.dart';
 import 'package:weather_app/data/my_data.dart';
+import 'package:geolocator/geolocator.dart';
 
 part 'weather_event.dart';
 part 'weather_state.dart';
